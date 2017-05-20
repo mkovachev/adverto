87c973e53571e34d86084af9cd3ec58850dab2f5
mkovachev <martin.kovachev@outlook.com>
2017-05-20 22:52:50 +0300

    minor fix

--- public/controllers/users-controller.js
+++ public/controllers/users-controller.js
@@ -1,13 +1,18 @@
 const usersController = function () {
 
     function login(context) {
-
+        templates.get('login')
+            .then(function (template) {
+                context.$element().html(template());
+            });
     }
 
     function register(context) {
         templates.get('register')
             .then(function (template) {
                 context.$element().html(template());
+
+
             });
     }
 
