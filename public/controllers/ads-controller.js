3fe33eaa48c420a38b7e8a4e49fcb9245cbc0b25
mkovachev <martin.kovachev@outlook.com>
2017-05-21 14:10:44 +0300

    added newAd func

--- public/controllers/ads-controller.js
+++ public/controllers/ads-controller.js
@@ -3,15 +3,32 @@ const adsController = function () {
     function all(context) {
         db.ads.get()
             .then(function (resAds) {
-                return templates.get('ads');
+                return templates.get('myAds');
             })
             .then(function (template) {
                 context.$element().html(template(ads));
-            })
+            });
     }
 
     function add(context) {
+        templates.get('newAd')
+            .then(function (template) {
+                context.$element().html(template());
 
+                const newAd = {
+                    title: $('#title').val(),
+                    category: $('#category').val(),
+                    description: $('#description').val(),
+                }
+
+                $('#btn-newAd').on('click', function () {
+                    db.ads.add(newAd)
+                        .then(function (newAd) {
+                            toastr.success(`${newAd.title} added!`);
+                            context.redirect('#/myads')
+                        });
+                });
+            })
     }
 
     return {
